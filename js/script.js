d9b84547f5f767549f8a49fd4d8c169077b73592
Djon05 <1955572@mail.ru>
2019-06-27 18:26:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-  var link = document.querySelector(".find-hotel-button");
-  var searchForm = document.querySelector(".hotel-form");
-  var date = searchForm.querySelector("#hotel-form-label");
+var button = document.querySelector(".find-hotel-button");
+var popup = document.querySelector(".modal");
+
+button.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   popup.classList.toggle("modal-show");
+   popup.classList.remove("modal-error");
+});
+
+var form = document.querySelector(".hotel-form");
+var input1 = document.querySelector(".input1");
+var input2 = document.querySelector(".input2");
+
+form.addEventListener("submit", function (evt) {
+   if (!input1.value || !input2.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+   }
+});
